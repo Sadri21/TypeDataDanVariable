64b13d03b5b53be6448c6d9857f8b7e61d8aa37c
Andi Sadri Ashari <sadriashari21@gmail.com>
2015-12-23 00:36:56 +0700

    Update list.py

--- list.py
+++ list.py
@@ -1,5 +1,5 @@
 print "belajar type data list"
-programLanguage [Java, C, Python, PHP, C#]
+programLanguage = ['Java', 'C', 'Python', 'PHP', 'C#']
 print programLanguage
-programLanguage[3] = HTML
+programLanguage[3] = 'HTML'
 print programLanguage
